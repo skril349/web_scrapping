5ecfd2ab4cfeaf58d43c586b735384afd8d16719
skril349 <tvives.cabaleiro@gmail.com>
2024-09-18 08:59:37 +0200

    adding parser to wikipedia page with lxml text_content

---
+++ wikipedia/wikipedia.py
@@ -0,0 +1,14 @@
+import requests
+from lxml import html
+
+encabezados = {
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+url = "https://www.wikipedia.org/"
+
+response = requests.get(url, headers = encabezados)
+
+parser = html.fromstring(response.text)
+
+ingles = parser.get_element_by_id("js-link-box-en")
+print(ingles.text_content())
